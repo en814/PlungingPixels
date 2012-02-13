e9fb3dc5fa75d4f1af78d9072541af369630e3f2
Ken Li <ken.li412@gmail.com>
2012-02-12 20:28:17 -0800

    Initial Commit

---
+++ PlungingPixels/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PlungingPixels
+//
+//  Created by Ken Li on 2/12/12.
+//  Copyright (c) 2012 Cal Poly San Luis Obispo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PlungingPixels/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PlungingPixels
+//
+//  Created by Ken Li on 2/12/12.
+//  Copyright (c) 2012 Cal Poly San Luis Obispo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
